45322dfd03b011e1d2cedf08b1afe95e7bdcb36d
Horfic <Horfic@7307f981-e087-44a5-86c2-13037a2ccb1f>
2010-01-16 17:44:59 +0000

    Added fetchmail plugin

---
+++ ispconfig3_fetchmail/fetchmail.js
@@ -0,0 +1,33 @@
+if (window.rcmail) {
+    rcmail.addEventListener('init', function(evt) {
+    
+    rcmail.register_command('plugin.ispconfig3_fetchmail.save', function() { 
+    	var input_server = rcube_find_object('_fetchmailserver');
+			var input_user = rcube_find_object('_fetchmailuser');
+			var input_pass = rcube_find_object('_fetchmailpass');
+    
+   		if(input_server.value == "" || input_user.value == "" || input_pass.value == ""){
+      	parent.rcmail.display_message(rcmail.gettext('textempty','ispconfig3_fetchmail'), 'error');    
+     	} else {
+	    	document.forms.fetchmailform.submit();
+    	}
+    }, true);
+  })
+}
+
+rcmail.register_command('plugin.ispconfig3_fetchmail.del', function(id){
+				if (confirm(rcmail.gettext('fetchmaildelconfirm','ispconfig3_fetchmail'))) {
+		    		rcmail.http_request('plugin.ispconfig3_fetchmail.del', '_id='+id, true);
+						row_del(id);
+	    		}
+
+				return false;
+			}, true);
+
+function edit(id){
+	window.location.href = '?_task=settings&_action=plugin.ispconfig3_fetchmail&_id='+id;
+}
+
+function row_del(id) {
+	document.getElementById('fetch-table').deleteRow(document.getElementById('fetch_'+id).rowIndex);
+}
