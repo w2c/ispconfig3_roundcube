ab810e833df0d0c6a7d7f27d12a3afc45419cbd4
Johannes Feichtner <johannes@web-wack.at>
2018-09-29 19:53:30 +0200

    forward: Plugin refactoring

--- ispconfig3_forward/forward.js
+++ ispconfig3_forward/forward.js
@@ -1,33 +1,39 @@
-if (window.rcmail) {
-    rcmail.addEventListener('init', function (evt) {
-        rcmail.register_command('plugin.ispconfig3_forward.save', function () {
-            var input_address = rcube_find_object('_forwardingaddress');
+window.rcmail && rcmail.addEventListener('init', function (evt) {
+    rcmail.register_command('plugin.ispconfig3_forward.save', function () {
+        var input_address = rcube_find_object('_forwardingaddress');
 
-            if (input_address.value == "") {
-                parent.rcmail.display_message(rcmail.gettext('forwardingempty', 'ispconfig3_forward'), 'error');
-            }
-            else {
-                if (!rcube_check_email(input_address.value, false)) {
-                    parent.rcmail.display_message(rcmail.gettext('invalidaddress', 'ispconfig3_forward'), 'error');
-                    input_address.focus();
-                }
-                else {
-                    document.forms.forwardform.submit();
-                }
-            }
-        }, true);
-    })
-}
+        if (input_address.value == '') {
+            parent.rcmail.display_message(rcmail.gettext('ispconfig3_forward.forwardingempty'), 'error');
+        }
+        else if (!rcube_check_email(input_address.value, false)) {
+            parent.rcmail.display_message(rcmail.gettext('ispconfig3_forward.invalidaddress'), 'error');
+            input_address.focus();
+        }
+        else {
+            document.forms.forward_form.submit();
+        }
+    }, true);
+    
+    rcmail.register_command('plugin.ispconfig3_forward.del', function (email) {
+        rcmail.confirm_dialog(rcmail.gettext('ispconfig3_forward.forwarddelconfirm'), 'delete', function(e, ref) {
+            var lock = ref.set_busy(true, 'loading');
+            ref.http_request('plugin.ispconfig3_forward.save', '_type=del&_forwardingaddress=' + email, lock);
+            document.getElementById('rule-table').deleteRow(document.getElementById('rule_' + email).rowIndex);
+        });
 
-rcmail.register_command('plugin.ispconfig3_forward.del', function (email) {
-    if (confirm(rcmail.gettext('forwarddelconfirm', 'ispconfig3_forward'))) {
-        rcmail.http_request('plugin.ispconfig3_forward.save', '_type=del&_forwardingaddress=' + email, true);
-        forward_row_del(email);
+        return false;
+    }, true);
+
+    if (rcmail.env.action.startsWith('plugin.ispconfig3_forward')) {
+        // Prevent implicit form submission using the enter key. Otherwise, one could bypass the input validation routines.
+        // See https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#implicit-submission
+        document.getElementById('forward_form').addEventListener('keypress', function(e) {
+            var key = rcube_event.get_keycode(e);
+            if (key === 13) {
+                e.preventDefault();
+            }
+        });
+
+        $('input:not(:hidden):first').focus();
     }
-
-    return false;
-}, true);
-
-function forward_row_del(email) {
-    document.getElementById('rule-table').deleteRow(document.getElementById('rule_' + email).rowIndex);
-}
+});
--- ispconfig3_forward/ispconfig3_forward.php
+++ ispconfig3_forward/ispconfig3_forward.php
@@ -2,42 +2,44 @@
 class ispconfig3_forward extends rcube_plugin
 {
     public $task = 'settings';
-    public $EMAIL_ADDRESS_PATTERN = '([a-z0-9][a-z0-9\-\.\+\_]*@[a-z0-9]([a-z0-9\-][.]?)*[a-z0-9]\\.[a-z]{2,5})';
+    private $rcmail;
     private $soap;
-    private $rcmail_inst;
 
     function init()
     {
-        $this->rcmail_inst = rcmail::get_instance();
-        $this->add_texts('localization/', true);
+        $this->rcmail = rcmail::get_instance();
+        $this->add_texts('localization/');
         $this->require_plugin('ispconfig3_account');
-
-        $this->soap = new SoapClient(null, array(
-            'location' => $this->rcmail_inst->config->get('soap_url') . 'index.php',
-            'uri' => $this->rcmail_inst->config->get('soap_url'),
-            'stream_context' => stream_context_create(array(
-                'ssl' => array(
-                    'verify_peer' => false,
-                    'verify_peer_name' => false,
-                    'allow_self_signed' => true
-                )
-            ))
-        ));
 
         $this->register_action('plugin.ispconfig3_forward', array($this, 'init_html'));
         $this->register_action('plugin.ispconfig3_forward.save', array($this, 'save'));
 
-        $this->api->output->add_handler('forward_form', array($this, 'gen_form'));
-        $this->api->output->add_handler('forward_table', array($this, 'gen_table'));
-        $this->api->output->add_handler('sectionname_forward', array($this, 'prefs_section_name'));
+        if (strpos($this->rcmail->action, 'plugin.ispconfig3_forward') === 0) {
+            $this->api->output->add_handler('forward_form', array($this, 'gen_form'));
+            $this->api->output->add_handler('forward_table', array($this, 'gen_table'));
+            $this->api->output->add_handler('sectionname_forward', array($this, 'prefs_section_name'));
 
-        $this->include_script('forward.js');
+            $this->include_script('forward.js');
+
+            $this->soap = new SoapClient(null, array(
+                'location' => $this->rcmail->config->get('soap_url') . 'index.php',
+                'uri' => $this->rcmail->config->get('soap_url'),
+                $this->rcmail->config->get('soap_validate_cert') ?:
+                    'stream_context' => stream_context_create(
+                        array('ssl' => array(
+                            'verify_peer' => false,
+                            'verify_peer_name' => false,
+                            'allow_self_signed' => true
+                        )
+                    ))
+            ));
+        }
     }
 
     function init_html()
     {
-        $this->rcmail_inst->output->set_pagetitle($this->gettext('acc_forward'));
-        $this->rcmail_inst->output->send('ispconfig3_forward.forward');
+        $this->rcmail->output->set_pagetitle($this->gettext('acc_forward'));
+        $this->rcmail->output->send('ispconfig3_forward.forward');
     }
 
     function prefs_section_name()
@@ -54,15 +56,14 @@ class ispconfig3_forward extends rcube_plugin
         else
             $address = strtolower(rcube_utils::get_input_value('_forwardingaddress', rcube_utils::INPUT_GET));
 
-        try
-        {
-            $session_id = $this->soap->login($this->rcmail_inst->config->get('remote_soap_user'), $this->rcmail_inst->config->get('remote_soap_pass'));
-            $mail_user = $this->soap->mail_user_get($session_id, array('login' => $this->rcmail_inst->user->data['username']));
+        try {
+            $session_id = $this->soap->login($this->rcmail->config->get('remote_soap_user'), $this->rcmail->config->get('remote_soap_pass'));
+            $mail_user = $this->soap->mail_user_get($session_id, array('login' => $this->rcmail->user->data['username']));
 
-            if ($address == $mail_user[0]['email'])
-                $this->rcmail_inst->output->command('display_message', $this->gettext('forwardingloop'), 'error');
-            else
-            {
+            if ($address == $mail_user[0]['email']) {
+                $this->rcmail->output->command('display_message', $this->gettext('forwardingloop'), 'error');
+            }
+            else {
                 $uid = $this->soap->client_get_id($session_id, $mail_user[0]['sys_userid']);
 
                 $params = $mail_user[0];
@@ -86,10 +87,10 @@ class ispconfig3_forward extends rcube_plugin
                     $params['autoresponder_start_date'] = $startdate;
                 }
 
-                if (empty($params['cc']))
+                if (empty($params['cc'])) {
                     $forward = $address;
-                else
-                {
+                }
+                else {
                     $forward = explode(",", $params['cc']);
                     while (($i = array_search($address, $forward)) !== false)
                         unset($forward[$i]);
@@ -105,42 +106,59 @@ class ispconfig3_forward extends rcube_plugin
                 $update = $this->soap->mail_user_update($session_id, $uid, $mail_user[0]['mailuser_id'], $params);
 
                 if ($type != 'del')
-                    $this->rcmail_inst->output->command('display_message', $this->gettext('successfullysaved'), 'confirmation');
+                    $this->rcmail->output->command('display_message', $this->gettext('successfullysaved'), 'confirmation');
                 else
-                    $this->rcmail_inst->output->command('display_message', $this->gettext('deletedsuccessfully'), 'confirmation');
+                    $this->rcmail->output->command('display_message', $this->gettext('deletedsuccessfully'), 'confirmation');
             }
 
             $this->soap->logout($session_id);
-        } catch (SoapFault $e)
-        {
-            $this->rcmail_inst->output->command('display_message', 'Soap Error: ' . $e->getMessage(), 'error');
+        }
+        catch (SoapFault $e) {
+            $this->rcmail->output->command('display_message', 'Soap Error: ' . $e->getMessage(), 'error');
         }
 
         $this->init_html();
     }
 
-    function gen_form()
+    function gen_form($attrib)
     {
-        $user = $this->rcmail_inst->user->get_prefs();
+        $this->rcmail->output->add_label('ispconfig3_forward.invalidaddress', 'ispconfig3_forward.forwardingempty');
 
-        $this->rcmail_inst->output->add_label('ispconfig3_forward.invalidaddress',
-            'ispconfig3_forward.forwardingempty');
+        $form_id = $attrib['id'] ?: 'form';
+        $out = $this->rcmail->output->request_form(array(
+                'id'      => $form_id,
+                'name'    => $form_id,
+                'method'  => 'post',
+                'task'    => 'settings',
+                'action'  => 'plugin.ispconfig3_forward.save',
+                'noclose' => true
+            ) + $attrib);
 
-        $this->rcmail_inst->output->set_env('framed', true);
+        $out .= '<fieldset><legend>' . $this->gettext('acc_forward') . '</legend>' . "\n";
 
-        $out = '<fieldset><legend>' . $this->gettext('acc_forward') . '</legend>' . "\n";
+        try {
+            $session_id = $this->soap->login($this->rcmail->config->get('remote_soap_user'), $this->rcmail->config->get('remote_soap_pass'));
+            $mail_user = $this->soap->mail_user_get($session_id, array('login' => $this->rcmail->user->data['username']));
+            $this->soap->logout($session_id);
 
-        $table = new html_table(array('cols' => 2, 'class' => 'propform'));
+            $field_id = 'forwardingaddress';
+            $input_forwardingaddress = new html_inputfield(array(
+                'name' => '_' . $field_id,
+                'id' => $field_id,
+                'value' => $mail_user[0]['cc'],
+                'maxlength' => 320,
+                'size' => 40
+            ));
 
-        $session_id = $this->soap->login($this->rcmail_inst->config->get('remote_soap_user'), $this->rcmail_inst->config->get('remote_soap_pass'));
-        $mail_user = $this->soap->mail_user_get($session_id, array('login' => $this->rcmail_inst->user->data['username']));
-        $this->soap->logout($session_id);
+            $table = new html_table(array('cols' => 2, 'class' => 'propform'));
+            $table->add('title', html::label($field_id, rcube::Q($this->gettext('forwardingaddress'))));
+            $table->add(null, $input_forwardingaddress->show());
+            $out .= $table->show();
+        }
+        catch (SoapFault $e) {
+            $this->rcmail->output->command('display_message', 'Soap Error: ' . $e->getMessage(), 'error');
+        }
 
-        $input_forwardingaddress = new html_inputfield(array('name' => '_forwardingaddress', 'id' => 'forwardingaddress', 'value' => $mail_user[0]['cc'], 'maxlength' => 320, 'size' => 40));
-        $table->add('title', rcube_utils::rep_specialchars_output($this->gettext('forwardingaddress')));
-        $table->add('', $input_forwardingaddress->show());
-
-        $out .= $table->show();
         $out .= "</fieldset>\n";
 
         return $out;
@@ -148,51 +166,45 @@ class ispconfig3_forward extends rcube_plugin
 
     function gen_table($attrib)
     {
-        $this->rcmail_inst->output->set_env('framed', true);
+        $this->rcmail->output->add_label('ispconfig3_forward.forwarddelconfirm');
 
         $out = '<fieldset><legend>' . $this->gettext('forward_entries') . '</legend>' . "\n";
 
         $rule_table = new html_table(array('id'    => 'rule-table',
                                            'class' => 'records-table', 'cellspacing' => '0', 'cols' => 2));
-        $rule_table->add_header("", $this->gettext('forward_entries'));
+        $rule_table->add_header(null, $this->gettext('forward_entries'));
         $rule_table->add_header(array('width' => '16px'), '');
 
-        try
-        {
-            $session_id = $this->soap->login($this->rcmail_inst->config->get('remote_soap_user'), $this->rcmail_inst->config->get('remote_soap_pass'));
-            $mail_user = $this->soap->mail_user_get($session_id, array('login' => $this->rcmail_inst->user->data['username']));
+        try {
+            $session_id = $this->soap->login($this->rcmail->config->get('remote_soap_user'), $this->rcmail->config->get('remote_soap_pass'));
+            $mail_user = $this->soap->mail_user_get($session_id, array('login' => $this->rcmail->user->data['username']));
             $this->soap->logout($session_id);
-            $class = 'odd';
 
-            $forward = explode(",", $mail_user[0]['cc']);
+            $forward = explode(',', $mail_user[0]['cc']);
+            if (!empty($forward[0])) {
+                for ($i = 0; $i < count($forward); $i++) {
+                    $row_attribs = array('id' => 'rule_' . $forward[$i]);
+                    if ($forward[$i] == rcube_utils::get_input_value('_forwardingaddress', rcube_utils::INPUT_GET)) {
+                        $row_attribs['class'] = 'selected';
+                    }
 
-            if (!empty($forward[0]))
-            {
-                for ($i = 0; $i < count($forward); $i++)
-                {
-                    $class = ($class == 'odd' ? 'even' : 'odd');
-
-                    if ($forward[$i] == rcube_utils::get_input_value('_forwardingaddress', rcube_utils::INPUT_GET))
-                        $class = 'selected';
-
-                    $rule_table->set_row_attribs(array('class' => $class, 'id' => 'rule_' . $forward[$i]));
+                    $rule_table->set_row_attribs($row_attribs);
                     $this->_rule_row($rule_table, $forward[$i], $attrib);
                 }
             }
-            else
-            {
-                $rule_table->add(array('colspan' => '2'), rcube_utils::rep_specialchars_output($this->gettext('forwardnomails')));
-                $rule_table->set_row_attribs(array('class' => 'odd'));
+            else {
+                $rule_table->add(array('colspan' => '2'), rcube::Q($this->gettext('forwardnomails')));
                 $rule_table->add_row();
             }
-        } catch (SoapFault $e)
-        {
-            $this->rcmail_inst->output->command('display_message', 'Soap Error: ' . $e->getMessage(), 'error');
+        }
+        catch (SoapFault $e) {
+            $this->rcmail->output->command('display_message', 'Soap Error: ' . $e->getMessage(), 'error');
         }
 
         $out .= "<div id=\"rule-cont\">" . $rule_table->show() . "</div>\n";
         $out .= "</fieldset>\n";
-
+        $out .= '</form>';
+        
         return $out;
     }
 
@@ -200,11 +212,10 @@ class ispconfig3_forward extends rcube_plugin
     {
         $rule_table->add(array('class' => 'rule'), $mail);
 
-        $del_button = $this->api->output->button(array('command' => 'plugin.ispconfig3_forward.del', 'prop' => $mail, 'type' => 'image',
-                                                       'image'   => $attrib['deleteicon'], 'alt' => $this->gettext('delete'),
-                                                       'title'   => $this->gettext('delete')));
-
-        $rule_table->add(array('class' => 'control'), $del_button);
+        $del_button = $this->api->output->button(array('command' => 'plugin.ispconfig3_forward.del', 'prop' => $mail,
+                                                       'type' => 'link', 'class' => 'button icon delete',
+                                                       'innerclass' => 'inner', 'content' => '', 'title' => 'delete'));
+        $rule_table->add(null, $del_button);
 
         return $rule_table;
     }
